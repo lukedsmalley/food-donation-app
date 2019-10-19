e2f21f06520b471d08f607e1c47e8f78613d2689
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2019-10-19 18:21:07 -0400

    Php files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CMS</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<div class="container">
+		<a href="index.php" id="logo">CMS</a>
+		<ol>
+			<li><a href="article.php?id=1">Article title</a></li>
+
+
+		</ol>
+
+
+	</div>
+
+</body>
+</html>
